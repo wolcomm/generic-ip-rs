85246d7e39bf2db8b36fbf73e424c1e4485dcf6b
Ben Maddison <benm@workonline.africa>
2023-04-21 19:17:56 +0200

    bump versions

--- src/lib.rs
+++ src/lib.rs
@@ -192,7 +192,7 @@
 //!     filtered
 //! );
 //! ```
-#![doc(html_root_url = "https://docs.rs/generic-ip/0.1.0-alpha.3")]
+#![doc(html_root_url = "https://docs.rs/generic-ip/0.1.0-rc.1")]
 // clippy lints
 #![warn(clippy::pedantic)]
 #![warn(clippy::cargo)]
